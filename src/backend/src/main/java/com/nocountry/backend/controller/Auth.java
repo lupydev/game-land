ea5d9b31d94581f22191a236e74b9f517494be41
Braian Marquez <braianmarq95@gmail.com>
2022-08-08 09:38:09 -0300

    mapeo

--- src/backend/src/main/java/com/nocountry/backend/controller/Auth.java
+++ src/backend/src/main/java/com/nocountry/backend/controller/Auth.java
@@ -1,4 +1,38 @@
 package com.nocountry.backend.controller;
 
+import com.nocountry.backend.dto.UserDTO;
+import com.nocountry.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class Auth {
+
+
+    public UserService userService;
+    @Autowired
+    public Auth(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> create(@RequestBody UserDTO userDTO){
+        UserDTO userSaved = userService.create(userDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userSaved);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> userFullId(@PathVariable Long id) {
+        UserDTO characterDTO = this.userService.findById(id);
+        return ResponseEntity.ok().body(characterDTO);
+    }
+    @GetMapping("/all")
+    public ResponseEntity<List<UserDTO>> getAll() {
+        List<UserDTO> users = userService.getAllUser();
+        return ResponseEntity.ok().body(users);
+    }
 }
