cd4f6b2444761627517e03879820195a70bbf828
LuMi <lumigu.dev@gmail.com>
2022-08-10 17:18:25 -0500

    se importan componentes y se agregan rutas

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Contact } from './components/Contact';
-import { Home } from './components/Home';
-import { About } from './components/About';
-import { Ranking } from './components/Ranking';
-import { Wordle } from './components/Wordle';
-import './style/App.css';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LandingPage } from "./components/LandingPage";
+import { Login } from "./components/Login";
+import { Registration } from "./components/Registration";
+import { Home } from "./components/Home";
+import { Contact } from "./components/Contact";
+import { About } from "./components/About";
+import { Ranking } from "./components/Ranking";
+import { Wordle } from "./components/Wordle";
+import "./style/App.css";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/ranking" element={<Ranking/>}/>
-          <Route path="/contact" element={<Contact/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/wordle" element={<Wordle/>}/>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/ranking" element={<Ranking />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/wordle" element={<Wordle />} />
         </Routes>
       </BrowserRouter>
     </>
