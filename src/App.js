67abb9175a7d9ca73af211796a976c110b453fe3
LuMi <lumigu.dev@gmail.com>
2022-08-10 16:22:16 -0500

    importe components y se agregaron rutas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Contact } from './components/Contact';
 import { Home } from './components/Home';
+import { About } from './components/About';
+import { Ranking } from './components/Ranking';
 import { Wordle } from './components/Wordle';
 import './style/App.css';
 
@@ -10,6 +13,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="/ranking" element={<Ranking/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/about" element={<About/>}/>
           <Route path="/wordle" element={<Wordle/>}/>
         </Routes>
       </BrowserRouter>
