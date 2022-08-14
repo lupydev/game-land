4768ce73074e1ca1a4843e19e997a23538560f81
LuMi <lumigu.dev@gmail.com>
2022-08-14 18:19:25 -0500

    se organizan parrafos en componente y se da estilo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 import { Header } from "./Header";
+import { NavBar } from "./NavBar";
 import { Footer } from "./Footer";
 
 export const About = () => {
   return (
     <div>
-        <Header/>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae
-        deserunt quasi quas tenetur, animi accusamus magni a consequatur, in est
-        ducimus consectetur eius corrupti, amet praesentium! Repudiandae ipsam
-        sapiente ad!
-        <br />
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio omnis
-        praesentium obcaecati consequatur excepturi et quisquam quis, ad
-        perspiciatis repudiandae sapiente quasi, quae aspernatur voluptatum
-        totam, tempora distinctio nemo sint.
-      </p>
-      <Footer/>
+      <Header />
+      <NavBar />
+      <div className="aboutContainer">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quia quidem ad enim, deserunt, omnis consequuntur ullam sit dolorum repellat dolorem dolor temporibus quas excepturi libero ipsum inventore, nesciunt reprehenderit!
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam eveniet incidunt in unde magnam iusto minus qui doloribus, explicabo dignissimos vel quisquam animi ipsam aperiam. Praesentium omnis repellendus magni necessitatibus.
+        </p>
+      </div>
+      <Footer />
     </div>
   );
 };
