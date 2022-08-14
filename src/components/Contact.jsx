5c0e176984593b1af7326362aa8508cf30d87717
LuMi <lumigu.dev@gmail.com>
2022-08-14 18:20:44 -0500

    se organizan las cards de contact y se le da estilo preliminar

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Header } from "./Header";
+import { NavBar } from "./NavBar";
 import { Footer } from "./Footer";
 
 export const Contact = () => {
   return (
     <div>
       <Header />
+      <NavBar />
+
       <div className="contactContainer">
         <div className="contactContainer-card">
           <div className="contactContainer-card--img">
@@ -13,11 +16,65 @@ export const Contact = () => {
           </div>
           <div className="contactContainer-card--contact">
             <p>GameLand</p>
-            <div
-              className="cont
-            
-            actContainer-card--socials"
-            ></div>
+            <div className="contactContainer-card--socials">
+            <i class="fa-brands fa-square-github"></i>
+            <i class="fa-brands fa-linkedin"></i>
+            </div>
+          </div>
+        </div><div className="contactContainer-card">
+          <div className="contactContainer-card--img">
+            <img src="" alt="" />
+          </div>
+          <div className="contactContainer-card--contact">
+            <p>GameLand</p>
+            <div className="contactContainer-card--socials">
+            <i class="fa-brands fa-square-github"></i>
+            <i class="fa-brands fa-linkedin"></i>
+            </div>
+          </div>
+        </div><div className="contactContainer-card">
+          <div className="contactContainer-card--img">
+            <img src="" alt="" />
+          </div>
+          <div className="contactContainer-card--contact">
+            <p>GameLand</p>
+            <div className="contactContainer-card--socials">
+            <i class="fa-brands fa-square-github"></i>
+            <i class="fa-brands fa-linkedin"></i>
+            </div>
+          </div>
+        </div><div className="contactContainer-card">
+          <div className="contactContainer-card--img">
+            <img src="" alt="" />
+          </div>
+          <div className="contactContainer-card--contact">
+            <p>GameLand</p>
+            <div className="contactContainer-card--socials">
+            <i class="fa-brands fa-square-github"></i>
+            <i class="fa-brands fa-linkedin"></i>
+            </div>
+          </div>
+        </div><div className="contactContainer-card">
+          <div className="contactContainer-card--img">
+            <img src="" alt="" />
+          </div>
+          <div className="contactContainer-card--contact">
+            <p>GameLand</p>
+            <div className="contactContainer-card--socials">
+            <i class="fa-brands fa-square-github"></i>
+            <i class="fa-brands fa-linkedin"></i>
+            </div>
+          </div>
+        </div><div className="contactContainer-card">
+          <div className="contactContainer-card--img">
+            <img src="" alt="" />
+          </div>
+          <div className="contactContainer-card--contact">
+            <p>GameLand</p>
+            <div className="contactContainer-card--socials">
+            <i class="fa-brands fa-square-github"></i>
+            <i class="fa-brands fa-linkedin"></i>
+            </div>
           </div>
         </div>
       </div>
