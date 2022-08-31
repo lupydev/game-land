f1561673879b336b0fd55f77a47ba1a495781fe8
LuMi <lumigu.dev@gmail.com>
2022-08-31 09:07:24 -0500

    se comenta ranking de juego, falta de tiempo no se desarrollo feature

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -25,28 +25,28 @@ export const Home = () => {
             <h2>Wordle</h2>
             <img src="https://i.postimg.cc/Y0ND6Dkt/WORDLE.png" alt="wordle" />
           </Link>
-          <div className="homeContainer-card--ranking">⭐⭐⭐⭐⭐</div>
+          {/* <div className="homeContainer-card--ranking">⭐⭐⭐⭐⭐</div> */}
         </div>
         <div className="homeContainer-card">
           <Link to="/sudoku">
             <h2>Sudoku</h2>
             <img src="https://i.postimg.cc/s2LZm548/SUDOKU.jpg" alt="sudoku" />
           </Link>
-          <div className="homeContainer-card--ranking">⭐⭐⭐⭐</div>
+          {/* <div className="homeContainer-card--ranking">⭐⭐⭐⭐</div> */}
         </div>
         <div className="homeContainer-card">
           <Link to="/buscaMinas">
             <h2>Busca Minas</h2>
             <img src="https://i.postimg.cc/wxN2ZjPZ/buscaminas.jpg" alt="busca minas" />
           </Link>
-          <div className="homeContainer-card--ranking">⭐⭐⭐⭐</div>
+          {/* <div className="homeContainer-card--ranking">⭐⭐⭐⭐</div> */}
         </div>
         <div className="homeContainer-card">
           <Link to="/memory">
             <h2>Memory</h2>
             <img src="https://i.postimg.cc/nh7KCRKz/memory.png" alt="memory game" />
           </Link>
-          <div className="homeContainer-card--ranking">⭐⭐⭐</div>
+          {/* <div className="homeContainer-card--ranking">⭐⭐⭐</div> */}
         </div>
       </div>
       <Footer />
