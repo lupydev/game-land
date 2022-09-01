136f29fef457814e714bc2d61c10b6a52d4659f7
LuMi <lumigu.dev@gmail.com>
2022-09-01 15:45:20 -0500

    descripcion game land

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,11 +9,12 @@ export const About = () => {
       <Header />
       <NavBar />
       <div className="aboutContainer">
+        <h2>Game land</h2>
         <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quia quidem ad enim, deserunt, omnis consequuntur ullam sit dolorum repellat dolorem dolor temporibus quas excepturi libero ipsum inventore, nesciunt reprehenderit!
+          La idea de crear un repositorio de mini juegos proviene de la necesidad de poder entrenar nuestro cerebro para despertar todo su potencial. Diferentes video juegos de memoria y concentraci&oacute;n nos ayudan a mejorar en nuestras habilidades blandas como <strong>soluci&oacute;n de problema, pensamiento critico y capacidad de adaptaci&oacute;n.</strong>
           <br />
           <br />
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam eveniet incidunt in unde magnam iusto minus qui doloribus, explicabo dignissimos vel quisquam animi ipsam aperiam. Praesentium omnis repellendus magni necessitatibus.
+          <strong>Game Land</strong> quiere transformarse en la herrameinta por excelencia de mejoramiento cognitivo del cerebro humano a nivel mundial.
         </p>
       </div>
       <Footer />
